38b24b6857f991ad13395f6ef62aa81952cc1826
Mike Lin <mlin@mlin.net>
2019-07-25 02:12:31 -0700

    working rewrite of cgranges

--- iitii.h
+++ iitii.h
@@ -3,107 +3,98 @@
 #include <algorithm>
 #include <assert.h>
 
+// class iitii template parameters:
+//   Pos  : integral position type (e.g. uint32_t, uint64_t)
+//   Item : arbitrary type of items to be indexed
+//   beg  : function to get interval begin position from Item
+//   end  : function to get interval end position from Item
+// For good performance, beg & end should just access members of Item (i.e. O(1) cache-local fetch)
 template<typename Pos, typename Item, Pos beg(const Item&), Pos end(const Item&)>
 class iitii {
-    static const Pos npos = std::numeric_limits<Pos>::max();
-    struct Node {
+    static const Pos npos = std::numeric_limits<Pos>::max();  // reserved constant for invalid Pos
+
+    struct Node {  // augmented item
         Item item;
-        Pos interior_max_end;  // max end of this & subtree (as in standard augmented interval tree)
-        Pos exterior_max_end;  // max end of all nodes n with A[n].beg <= this->beg, excluding this & subtree
-        Pos exterior_min_beg;  // min beg of all nodes n with A[n].beg >= this->beg, excluding this & subtree
+        Pos inside_max_end;   // max end of this & subtree (as in textbook augmented interval tree)
+        Pos outside_max_end;  // max end of all nodes n with A[n].beg <= this->beg, excluding this & subtree
+        Pos outside_min_beg;  // min beg of all nodes n with A[n].beg >= this->beg, excluding this & subtree
 
         Node(const Item& item_)
             : item(item_)
-            , interior_max_end(std::numeric_limits<Pos>::min())
-            , exterior_max_end(std::numeric_limits<Pos>::min())
-            , exterior_min_beg(npos)
+            , inside_max_end(end(item_))
+            , outside_max_end(std::numeric_limits<Pos>::min())
+            , outside_min_beg(npos)
             {}
     };
-    std::vector<Node> A;
-    typedef std::size_t Rank;
-    typedef std::size_t Level;
-    size_t nsz = std::numeric_limits<size_t>::max();
-    Rank root;
-    Level root_level;          // K
-    Pos max_end;
-    Pos min_beg, max_beg;      // for interpolation
+    std::vector<Node> A;       // sorted array of Nodes
 
+    // beg & end extreme values in A
+    Pos min_beg, max_beg;
+
+    // type aliases Rank and Level, just to help keep these concepts straight (& Pos)
+    typedef std::size_t Rank;  // rank of a node, or its index in A
+    Rank root;
+    static const Rank nrank = std::numeric_limits<Rank>::max();  // reserved constant for invalid Rank
+    typedef std::size_t Level;
+    Level root_level;          //  = K in cgranges
+
+    // compute a node's level, the # of 1 bits below the lowest 0 bit
     inline Level level(Rank node) const {
-        Level ans;
-        for (ans=0; node&1; ans++, node>>=1);
+        assert(node < (Rank(1)<<(root_level+1))-1);
+        Level ans = __builtin_ctzll(~node);
+        #ifndef NDEBUG
+        Level chk;
+        for (chk=0; node&1; chk++, node>>=1);
+        assert(ans == chk);
+        #endif
         return ans;
     }
 
-    inline Rank parent(Rank node, Level level) const {
-        assert(node != root);
-        Rank ofs = Rank(1) << level;
+    inline Rank parent(Rank node) const {
+        assert(node < ((Rank(1)<<(root_level+1))-1));
+        if (node == root) {
+            return nrank;
+        }
+        Level lv = level(node);
+        Rank ofs = Rank(1) << lv;
         assert(node >= ofs-1);
-        if (((node>>(level+1)) & 1)) {
+        if (((node>>(lv+1)) & 1)) {
             assert(node >= ofs);
             return node-ofs;
         }
         return node+ofs;
     }
 
-    inline Rank left(Rank node, Level lv) const {
-        assert(lv && lv == level(node));
-        return node - (Rank(1) << (lv-1));
-    }
-
     inline Rank left(Rank node) const {
-        return left(node, level(node));
-    }
-
-    inline Rank right(Rank node, Level lv) const {
-        assert(lv && lv == level(node));
-        return node + (Rank(1) << (lv-1));
+        Level lv = level(node);
+        return lv > 0 ? node - (Rank(1) << (lv-1)) : nrank;
     }
 
     inline Rank right(Rank node) const {
-        return right(node, level(node));
+        Level lv = level(node);
+        return lv > 0 ? node + (Rank(1) << (lv-1)) : nrank;
     }
 
-    inline Pos effective_interior_max_end(Rank node) const {
-        if (node >= A.size()) {
-            return max_end;
+    size_t scan(Rank subtree, Pos qbeg, Pos qend, std::vector<Item>& ans) const {
+        if (subtree == nrank) {
+            return 0;
         }
-        return A[node].interior_max_end;
-    }
-
-    inline Pos effective_exterior_max_end(Rank node) const {
-        if (node >= A.size()) {
-            // FIXME what to put here?
+        if (subtree >= A.size()) {
+            return 1 + scan(left(subtree), qbeg, qend, ans);
         }
-        return A[node].exterior_max_end;
-    }
-
-    inline Pos effective_exterior_min_beg(Rank node) const {
-        if (node >= A.size()) {
-            // FIXME what to put here?
-        }
-        return A[node].exterior_min_beg;
-    }
-
-    void scan(Rank subtree, Level lv, Pos qbeg, Pos qend, std::vector<Item>& ans) const {
-        assert(lv == level(subtree));
-        if (effective_interior_max_end(subtree) > qbeg) {
-            if (lv) {
-                scan(left(subtree, lv), lv-1, qbeg, qend, ans);
-            }
-            if (subtree < A.size()) {
-                const Node& n = A[subtree];
-                Pos begn = beg(n.item);
-                if (begn < qend && qbeg < end(n.item)) {
+        size_t cost = 1;
+        const Node& n = A[subtree];
+        if (n.inside_max_end > qbeg) {
+            cost += scan(left(subtree), qbeg, qend, ans);
+            Pos nbeg = beg(n.item);
+            if (nbeg < qend) {
+                if (qbeg < end(n.item)) {
                     ans.push_back(n.item);
                 }
-                if (lv && begn < qend) {
-                    scan(right(subtree, lv), lv-1, qbeg, qend, ans);
-                }
-            } else if (lv) {
-                // TODO: ghost node stopping condition
-                scan(right(subtree, lv), lv-1, qbeg, qend, ans);
+                cost += scan(right(subtree), qbeg, qend, ans);
             }
         }
+        return cost;
     }
 
     Rank interpolate(Pos q) const {
@@ -123,45 +114,64 @@ public:
         , root(std::numeric_limits<Rank>::max())
         , min_beg(npos)
         , max_beg(std::numeric_limits<Pos>::min())
-        , max_end(std::numeric_limits<Pos>::min())
     {
         std::for_each(first, last, [&](const Item& it) {
-            Pos beg_it = beg(it), end_it = end(it);
+            Pos beg_it = beg(it);
             min_beg = std::min(min_beg, beg_it);
             max_beg = std::max(max_beg, beg_it);
-            max_end = std::max(max_end, end_it);
             A.push_back(Node(it));
-            (A.end()-1)->interior_max_end = end_it;
         });
-        std::sort(A.begin(), A.end(), [](const Node& lhs, const Node& rhs) { return beg(lhs.item) < beg(rhs.item); });
         if (A.size()) {
+            std::sort(A.begin(), A.end(),
+                  [](const Node& lhs, const Node& rhs) { return beg(lhs.item) < beg(rhs.item); });
+
             for (root_level = 0; (Rank(1)<<(root_level+1))-1 < A.size(); root_level++);
             root = (Rank(1) << root_level) - 1;
-        }
-        for (Level lv=1; lv <= root_level; ++lv) {
-            size_t x = size_t(1)<<(lv-1), step = x<<2;
-            for (Rank n = (x<<1)-1; n < A.size(); n += step) {
-                A[n].interior_max_end = std::max(A[n].interior_max_end, effective_interior_max_end(left(n, lv)));
-                A[n].interior_max_end = std::max(A[n].interior_max_end, effective_interior_max_end(right(n, lv)));
+            
+            Rank rightmost_leaf = A.size() - (2 - A.size() % 2);
+            Rank rightmost_anc = rightmost_leaf;
+            Pos rightmost_ime = A[rightmost_anc].inside_max_end;
+            for (Level lv=1; lv <= root_level; ++lv) {
+                size_t x = size_t(1)<<(lv-1), step = x<<2;
+                for (Rank n = (x<<1)-1; n < A.size(); n += step) {
+                    Pos ime = A[n].inside_max_end;
+                    ime = std::max(ime, A[left(n)].inside_max_end);
+                    if (right(n) < A.size()) {
+                        ime = std::max(ime, A[right(n)].inside_max_end);
+                    } else {
+                        ime = std::max(ime, rightmost_ime);
+                    }
+                    A[n].inside_max_end = ime;
+                }
+                rightmost_anc = parent(rightmost_anc);
+                if (rightmost_anc < A.size()) {
+                    rightmost_ime = A[rightmost_anc].inside_max_end;
+                }
             }
         }
-        // FIXME populate exterior_max_end & exterior_min_beg
+        // FIXME populate outside_max_end & outside_min_beg
     }
 
-    void overlap(Pos qbeg, Pos qend, std::vector<Item>& ans) const {
+    size_t overlap(Pos qbeg, Pos qend, std::vector<Item>& ans) const {
         ans.clear();
-        scan(root, root_level, qbeg, qend, ans);
+        return scan(root, qbeg, qend, ans);
     }
 
-    void overlap_interpolate(Pos qbeg, Pos qend, std::vector<Item>& ans) const {
+    std::vector<Item> overlap(Pos qbeg, Pos qend) const {
+        std::vector<Item> ans;
+        scan(root, qbeg, qend, ans);
+        return ans;
+    }
+    /*
+    size_t overlap_interpolate(Pos qbeg, Pos qend, std::vector<Item>& ans) const {
         Rank subtree = interpolate(qbeg);
         Level level = 0;
-        while (level < root_level && !(effective_exterior_max_end(subtree) <= qbeg &&
-                                       qend <= effective_exterior_min_beg(subtree))) {
+        while (level < root_level && !(effective_outside_max_end(subtree) <= qbeg &&
+                                       qend <= effective_outside_min_beg(subtree))) {
             subtree = parent(subtree, level++);
         }
 
         ans.clear();
-        scan(qbeg, qend, ans);
-    }
+        return scan(qbeg, qend, ans);
+    } */
 };
--- test/test_iitii.cc
+++ test/test_iitii.cc
@@ -1,28 +1,82 @@
 #include <iostream>
 #include <utility>
+#include <random>
 #include "iitii.h"
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 
 using namespace std;
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+typedef uint32_t pos;
+typedef pair<pos, pos> pospair;
 
-uint32_t iitii_beg(const pair<uint32_t, uint32_t>& p) {
+pos iitii_beg(const pospair& p) {
     return p.first;
 }
 
-uint32_t iitii_end(const pair<uint32_t, uint32_t>& p) {
+pos iitii_end(const pospair& p) {
     return p.second;
 }
 
-TEST_CASE("cgranges example") {
-    vector<pair<uint32_t, uint32_t>> examples = { { 12, 34 }, { 0, 23 }, { 34, 56 } };
-    iitii<uint32_t, pair<uint32_t, uint32_t>, &iitii_beg, &iitii_end> tree(examples.begin(), examples.end());
+auto build_tree(const vector<pospair>& examples) {
+    return iitii<pos, pospair, &iitii_beg, &iitii_end>(examples.begin(), examples.end());
+}
 
-    examples.clear();
-    tree.overlap(22, 25, examples);
-    REQUIRE(examples.size() == 2);
+TEST_CASE("cgranges example") {
+    auto tree = build_tree({ { 12, 34 }, { 0, 23 }, { 34, 56 } });
+
+    auto results = tree.overlap(22, 25);
+    REQUIRE(results.size() == 2);
+    REQUIRE(results[0].first == 0);
+    REQUIRE(results[1].first == 12);
+}
+
+TEST_CASE("ghost nodes (N=5)") {
+    // the three rank levels are:
+    // 2.       3
+    // 1.     1   5
+    // 0.    0 2 4 6
+    // with N=5 (ranks 0-4), nodes 5 and 6 are "ghosts"; nb ghost node 5 is the parent of 4
+    //
+    // lets set up an example where node 4 shall be part of the result set
+
+    vector<pospair> examples = { { 0, 7 }, {1, 2}, {2, 4}, {3, 6}, {4, 9} };
+    iitii<pos, pospair, &iitii_beg, &iitii_end> tree(examples.begin(), examples.end());
+
+    auto results = tree.overlap(6, 10);
+    REQUIRE(results.size() == 2);
+    REQUIRE(results[0].first == 0);
+    REQUIRE(results[1].first == 4);
+}
+
+TEST_CASE("fuzz") {
+    default_random_engine R(42);
+    uniform_int_distribution<uint32_t> begD(1, 42000);
+    geometric_distribution<uint16_t> lenD(0.01);
+    size_t count = 0, cost = 0;
+
+    for (int N = 3; N < 500000; N *= 3) {
+        vector<pospair> examples;
+        for (int i = 0; i < N; ++i) {
+            auto beg = begD(R);
+            examples.push_back({ beg, beg+lenD(R) });
+        }
+        iitii<pos, pospair, &iitii_beg, &iitii_end> tree(examples.begin(), examples.end());
+        for (int i = 0; i < 10000; ++i) {
+            auto qbeg = begD(R);
+            auto qend = qbeg + 100;
+            vector<pospair> results;
+            cost += tree.overlap(qbeg, qend, results);
+
+            vector<pospair> results2;
+            for (const auto& p : examples) {
+                if (qbeg < p.second && p.first < qend) {
+                    results2.push_back(p);
+                }
+            }
+            REQUIRE(results.size() == results2.size());
+            count += results.size();
+        }
+    }
+    cout << "fuzz count = " << count << ", cost = " << cost << endl;
 }
