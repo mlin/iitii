55ead32c34f9fe79a0dc41693cdb070d16a7b8de
Erik Garrison <erik.garrison@gmail.com>
2019-10-15 16:46:16 +0200

    switch to construction from vector

--- example.cc
+++ example.cc
@@ -9,12 +9,13 @@ int p_get_end(const intpair& p) { return p.second; }
 using p_iitii = iitii::iitii<int, intpair, p_get_beg, p_get_end>;  // first arg is position type
 
 int main() {
-    p_iitii::builder br;
-    br.add(intpair(12,34));
-    br.add(intpair(0,23));
-    br.add(intpair(34,56));
-    p_iitii db = br.build(1);  // 1 = # domains
+    std::vector<intpair> c;
+    c.push_back(intpair(12,34));
+    c.push_back(intpair(0,23));
+    c.push_back(intpair(34,56));
+    //p_iitii db = br.build(1);  // 1 = # domains
     // alternative: p_iitii db = p_iitii::builder(container.begin(), container.end()).build(1);
+    p_iitii db = p_iitii::builder(c.begin(), c.end()).build(1);
 
     std::vector<intpair> results = db.overlap(22, 25);
     // alternative: db.overlap(22, 25, results);
